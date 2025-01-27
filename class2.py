71b1049884a32bd000ecbd400296e806648e212b
Muhammad Sufiyan <148213204+SufiyanSheikh0321@users.noreply.github.com>
2025-01-27 19:03:56 +0500

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,53 @@
+
+# print Hello World
+
+print("Hello World")
+
+
+
+# DATA Types
+
+s = "Hello World"  # String
+b = True           # Boolean
+i = 18             # Integer
+f = 20.2           # Float
+c = 1j             #Complex
+l = ["Apple", "Mango", "Banana", "Cherry"]          # List
+t = ("Peach", "Black Berry", "PineApple", "Grapes") # Tuple
+d = {"name" : "Sufiyan" , "Age" : 18}               # Dict
+
+print(s)
+print(b)
+print(i)
+print(f)
+print(l)
+print(t)
+print(d)
+
+
+
+
+# Type
+
+print(type(s))
+print(type(b))
+print(type(i))
+print(type(f))
+print(type(l))
+print(type(t))
+print(type(d))
+
+
+
+
+# Concatination
+
+print("Hello " + "World")
+
+
+
+# Concatination Number
+num1 = 10
+num2 = 20
+
+print(num1 + num2)
